11914471d74153cde027894981225ad2f10a38f2
Rinat <rinat-12-05-01@mail.ru>
2021-08-17 21:15:54 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+"use strict";
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+//alert("Hello");
+
+//const result = confirm("Are you here?");
+/*
+const answer = prompt("Вам есть 18?", "18");
+
+console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имЯ?','');
+answers[1] = prompt('Как ваше фамилия?','');
+answers[2] = prompt('Сколько вам лет?','');
+
+document.write(answers);*/
+/*
+console.log(-4/0);
+
+console.log('string' * 9);
+
+const da = true;
+
+console.log(da);
+
+console.log(1);
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj ={
+    name:'John',
+    age: 25,
+    isMarried: false
+};
+console.log(obj["name"]);
+obj.a = 10;
+
+console.log(obj.a);*/
