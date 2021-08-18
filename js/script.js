b37aa7b868892966f7da893bc8e7cf437f6af8fa
Rinat <rinat-12-05-01@mail.ru>
2021-08-18 23:07:36 +0600

    practice_start

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 "use strict";
+const numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+const answers = [];
+const  personalMovieDB ={
+    count: numberofFilms,
+    movies:{},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
 
+personalMovieDB.movies[a] =b;
+personalMovieDB.movies[c] =d;
 
-
-
+console.log(personalMovieDB);
 /*const category = 'toys';
 console.log(`https://someurl.com/${category}/5`);
 
