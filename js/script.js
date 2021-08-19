b1d06f6056ae7e6dd5198f95cb05593b2a453813
Rinat <rinat-12-05-01@mail.ru>
2021-08-19 21:37:51 +0600

    second practice

--- js/script.js
+++ js/script.js
@@ -8,6 +8,89 @@ const  personalMovieDB ={
     genres: [],
     privat: false
 };
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if(a != null && b!= null && a != '' && b != '' && a.length < 50 ){
+        personalMovieDB.movies[a] = b; 
+        console.log('DONE');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+/*
+let i = 0;
+while (i<2){
+    
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if(a != null && b!= null && a != '' && b != '' && a.length < 50 ){
+        personalMovieDB.movies[a] = b; 
+        console.log('DONE');
+        i++;
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+do {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if(a != null && b!= null && a != '' && b != '' && a.length < 50 ){
+        personalMovieDB.movies[a] = b; 
+        console.log('DONE');
+        i++;
+    } else {
+        console.log('error');
+        i--;
+    }
+} while(i<2);*/
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+console.log(personalMovieDB);
+
+/*
+let i = 0;
+while (i < 2) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+    if (a.length == 0) {
+        alert("Вы не ввели название фильма");
+        i--;
+        break;
+    } else if (a.length > 50) {
+        alert("Название фильмо не может привыщать 50 симмволов");
+        i--;
+        break;
+    }
+    personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+
+/*
 const a = prompt('Один из последних просмотренных фильмов?',''),
       b = prompt('На сколько оцените его?',''),
       c = prompt('Один из последних просмотренных фильмов?',''),
@@ -17,7 +100,25 @@ personalMovieDB.movies[a] =b;
 personalMovieDB.movies[c] =d;
 
 console.log(personalMovieDB);
-/*const category = 'toys';
+
+
+/*
+let  num =50;
+do{
+    console.log(num);
+    num++;
+} 
+while (num<55);
+
+while (num<=55){
+    console.log(num);
+    num++;
+}
+
+for (let i = 1; i<8; i++){
+
+}
+const category = 'toys';
 console.log(`https://someurl.com/${category}/5`);
 
 //alert("Hello");
